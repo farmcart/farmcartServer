8cad22ffe130759871d0254636c31145f13ed934
farmcart <kb4shubhamyahoo@gmail.com>
2018-02-19 16:37:13 +0530

    Initial commit

---
+++ app/src/main/java/com/farmkart/common/Common.java
@@ -0,0 +1,12 @@
+package com.farmkart.common;
+
+import com.farmkart.model.User;
+
+/**
+ * Created by kumar on 2/17/2018.
+ */
+
+public class Common {
+
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/farmkart/model/User.java
@@ -0,0 +1,50 @@
+package com.farmkart.model;
+
+/**
+ * Created by kumar on 2/17/2018.
+ */
+
+public class User {
+
+    private String Name, Password, Phone, isStaff;
+
+    public User(String name, String password) {
+        Name = name;
+        Password = password;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getIsStaff() {
+        return isStaff;
+    }
+
+    public void setIsStaff(String isStaff) {
+        this.isStaff = isStaff;
+    }
+}
